d86ca38cc3556b0adbdcc37578f3e8f06ef67123
belochka-228 <sofikrohmal@gmail.com>
2026-04-27 22:08:17 +0300

    Буквы

--- practice7/Program.cs
+++ practice7/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace practice7
+class ArrayExample
 {
-    internal class Program
+    /// <summary>
+    /// Главный метод программы
+    /// </summary>
+    static void Main()
     {
-        static void Main(string[] args)
+        char[] letters = { 'f', 'r', 'e', 'd', ' ', 's', 'm', 'i', 't', 'h' };
+        string name = "";
+        int[] a = new int[10];
+        for (int i = 0; i < letters.Length; i++)
         {
+            name += letters[i];
+            a[i] = i + 1;
+            SendMessage(name, a[i]);
         }
+        Console.ReadKey();
+    }
+    /// <summary>
+    /// Выводит приветствие и число
+    /// </summary>
+    static void SendMessage(string name, int msg)
+    {
+        Console.WriteLine("Hello, " + name + "! Count to " + msg);
     }
 }
