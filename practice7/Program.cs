87306a6b7f8740ad0c2527021800d3fc20e6e225
belochka-228 <sofikrohmal@gmail.com>
2026-04-27 21:54:57 +0300

    отладка приложения Фибоначчи

--- practice7/Program.cs
+++ practice7/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace practice7
+namespace pr7
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int result = Fibonacci(5);
+            Console.WriteLine(result);
+        }
+
+        /// <summary>
+        /// Вычисляет n-е число последовательности Фибоначчи
+        /// </summary>
+        /// <param name="n">Порядковый номер числа Фибоначчи</param>
+        /// <returns>n-е число Фибоначчи</returns>
+        static int Fibonacci(int n)
+        {
+            Console.WriteLine("The output is: ");
+            int n1 = 0;
+            int n2 = 1;
+            int sum;
+
+            for (int i = 2; i <= n; i++)
+            {
+                sum = n1 + n2;
+                n1 = n2;
+                n2 = sum;
+            }
+
+            return n == 0 ? n1 : n2;
         }
     }
 }
